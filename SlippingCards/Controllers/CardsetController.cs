4ecbe247b4d9b48d2601152572d2a10cfc94b8c4
Iván E. Sierra <ivan@izzitech.com.ar>
2020-10-28 19:53:11 -0300

    Now it shows cards. It's showing file SlippingCards_template.txt

---
+++ SlippingCards/Controllers/CardsetController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SlippingCards.HelperClasses;
+using SlippingCards.Models;
+
+namespace SlippingCards.Controllers
+{
+    public class CardsetController : Controller
+    {
+        public IActionResult Create()
+        {
+            TextReader cardsetTemplate = 
+                new StreamReader(
+                    Path.Combine(
+                        AppDomain.CurrentDomain.BaseDirectory,
+                        "SlippingCards_template.txt"));
+            var cardset = CardsetLoaderHelper.LoadCardset(cardsetTemplate.ReadToEnd());
+            return View(cardset);
+        }
+    }
+}
