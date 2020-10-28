58638ad5b62b3844ab824a6105c90d50b2b64f9a
Iván E. Sierra <ivan@izzitech.com.ar>
2020-10-28 09:32:31 -0300

    Modified homescreen

---
+++ SlippingCards/ViewModel/CardLoaderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlippingCards.ViewModel
+{
+    public class CardLoaderViewModel
+    {
+        [DisplayName("Cardset data")]
+        [DataType(DataType.MultilineText)]
+        public string CardSetText { get; set; }
+    }
+}
